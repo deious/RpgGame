368de3c977786da28a99dd95738966c374677e89
deious <gkstjddlekd@gmail.com>
2020-12-25 23:27:55 +0900

    바람 효과를 위한 WindZone 추가

--- RpgGame/Assets/Effect Assets/DynamicElements_Effects/scripts/ParticleScale.js
+++ RpgGame/Assets/Effect Assets/DynamicElements_Effects/scripts/ParticleScale.js
@@ -4,13 +4,13 @@ var particleScale:float=1;
 
 function Start () {
 
-GetComponent.<ParticleEmitter>().minSize*=particleScale;
-GetComponent.<ParticleEmitter>().maxSize*=particleScale;
-GetComponent.<ParticleEmitter>().worldVelocity*=particleScale;
-GetComponent.<ParticleEmitter>().localVelocity*=particleScale;
-GetComponent.<ParticleEmitter>().rndVelocity*=particleScale;
-GetComponent.<ParticleEmitter>().angularVelocity*=particleScale;
-GetComponent.<ParticleEmitter>().rndAngularVelocity*=particleScale;	
+//GetComponent.<ParticleEmitter>().minSize*=particleScale;
+//GetComponent.<ParticleEmitter>().maxSize*=particleScale;
+//GetComponent.<ParticleEmitter>().worldVelocity*=particleScale;
+//GetComponent.<ParticleEmitter>().localVelocity*=particleScale;
+//GetComponent.<ParticleEmitter>().rndVelocity*=particleScale;
+//GetComponent.<ParticleEmitter>().angularVelocity*=particleScale;
+//GetComponent.<ParticleEmitter>().rndAngularVelocity*=particleScale;	
 
 
 }
